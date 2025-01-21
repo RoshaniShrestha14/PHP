86612c40e811f9ce145095ea4c6f9b39b21880b0
Roshani2211205 <roshani2211205@iimscollege.edu.np>
2025-01-21 08:05:10 +0545

    jj

---
+++ notes.php
@@ -0,0 +1,15 @@
+<?php
+include 'Database.php'; 
+include 'utils.php';
+
+$db = new Database();
+
+$statement =$db->query("SELECT * FROM note");
+
+$notes=$statement->fetchAll();
+dd($notes);
+
+
+
+$navTitle="Notes";
+include 'views/notes.view.php';
